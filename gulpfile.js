156228d254c7ff246b4b34bb6949b431f05b66c3
D14na <support@d14na.org>
2018-10-02 05:27:45 -0400

    Removed errors from gulp. TODO: Write first "real" test.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-const codecov = require('gulp-codecov')
+// const codecov = require('gulp-codecov')
 const gulp = require('gulp')
 
-gulp.task('default', function() {
+gulp.task('default', function(_cb) {
     // place code for your default task here
+    _cb()
 })
 
-gulp.src('./coverage/lcov.info').pipe(codecov())
+// gulp.src('./coverage/lcov.info').pipe(codecov())
